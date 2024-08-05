d0b081c60237e221279911f56d51a930a3cefbc4
Lewis Foot <lfoot91@hotmail.co.uk>
2024-08-05 17:54:38 +0100

    feat: finished module 3

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -4,7 +4,8 @@ using PublisherDomain;
 
 using (PubContext pubContext = new PubContext())
 {
-    pubContext.Database.EnsureCreated();
+    //EF core will check to ensure database is creted and create if it doesn't
+    //pubContext.Database.EnsureCreated();
 
     //AddAuthor();
     GetAuthors();
@@ -27,11 +28,25 @@ void AddAuthor()
 void GetAuthors()
 {
     var context = new PubContext();
-    var authors = context.Authors.Include(x => x.Books).ToList();
+    var authors = context.Authors
+        .Include(x => x.Books)
+        .Where(x => x.FirstName == "Lewis")
+        .ToList();
+
+    var lewiAuth = context.Authors
+        .Include(x => x.Books)
+        .Where(x => EF.Functions.Like(x.FirstName, "L%"))
+        .ToList()
+        .OrderBy(x => x.LastName)
+        .ThenBy(x => x.FirstName);
 
     foreach (var author in authors)
     {
         Console.WriteLine(author.FirstName);
         Console.WriteLine(authors[1].Books.FirstOrDefault()!.Title);
     }
+
+    var lewisAuthor = authors.FirstOrDefault(x => x.FirstName == "Lewis");
+
+    //Skip and Take LINQ query can be useful for paging, getting a specific number of values returned
 }
