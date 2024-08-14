ac55a3ca9a2b8b2d8b1261446bbe5ce27bc7aaa4
Lewis Foot <lfoot91@hotmail.co.uk>
2024-08-14 19:16:09 +0100

    feat: finished module 6 and 7

---
+++ PublishData/Migrations/20240814173850_authoridchanged.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PublishData.Migrations
+{
+    /// <inheritdoc />
+    public partial class authoridchanged : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Authors",
+                newName: "AuthorId");
+
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "AuthorId", "BasePrice", "PublishDate", "Title" },
+                values: new object[] { 1, 1, 0m, new DateOnly(1996, 4, 13), "Lewis Book" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.RenameColumn(
+                name: "AuthorId",
+                table: "Authors",
+                newName: "Id");
+        }
+    }
+}
--- PublisherDomain/Author.cs
+++ PublisherDomain/Author.cs
@@ -2,10 +2,10 @@
 {
     public class Author
     {
-        public int Id { get; set; }
+        public int AuthorId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public List<Book> Books 
-          { get; set; } = new List<Book>();
+        public List<Book> Books
+        { get; set; } = new List<Book>();
     }
 }
